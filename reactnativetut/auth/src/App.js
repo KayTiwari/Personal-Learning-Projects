e47f54e6288b44aa38c15e836ae545715bd37c89
Abhinav Tiwari <abhi.kay.tiwari@gmail.com>
2019-04-20 12:10:19 -0600

    added firebase setup

--- reactnativetut/auth/src/App.js
+++ reactnativetut/auth/src/App.js
@@ -1,14 +1,30 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import firebase from 'firebase';
 import { Header } from './components/common';
 
-export default class App extends Component {
-  render() {
-    return (
-      <View>
-          <Header text={'Wild5'}/>
-          <Text>Hi</Text>
-      </View>
-    )
+
+
+class App extends Component {
+    componentWillMount() {
+        firebase.initializeApp({
+            apiKey: 'AIzaSyC0Xfz3qA33dg80pgnu5JR7Lc-Z1i2-uI0',
+            authDomain: 'authenticationprac.firebaseapp.com',
+            databaseURL: 'https://authenticationprac.firebaseio.com',
+            projectId: 'authenticationprac',
+            storageBucket: 'authenticationprac.appspot.com',
+            messagingSenderId: '409600324634'
+          })
+    }
+
+
+    render() {
+        return (
+        <View>
+            <Header text={'Wild5'}/>
+            <Text>Hi</Text>
+        </View>
+        )
   }
 }
+export default App
