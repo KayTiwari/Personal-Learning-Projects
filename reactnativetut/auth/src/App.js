bfd541a0317f19544e5313e307ef0844f59e44e4
Abhinav Tiwari <abhi.kay.tiwari@gmail.com>
2019-04-20 14:25:49 -0600

    added logout feature

--- reactnativetut/auth/src/App.js
+++ reactnativetut/auth/src/App.js
@@ -1,12 +1,17 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View } from 'react-native';
 import firebase from 'firebase';
-import { Header } from './components/common';
+import { Header, Button, Spinner, CardSection } from './components/common';
 import LoginForm from './components/LoginForm'
 
 
 
 class App extends Component {
+
+    state = {
+        loggedIn: null,
+    }
+
     componentWillMount() {
         firebase.initializeApp({
             apiKey: 'AIzaSyC0Xfz3qA33dg80pgnu5JR7Lc-Z1i2-uI0',
@@ -16,14 +21,37 @@ class App extends Component {
             storageBucket: 'authenticationprac.appspot.com',
             messagingSenderId: '409600324634'
           })
+        firebase.auth().onAuthStateChanged((user) => {
+            if (user){
+                this.setState({
+                    loggedIn: true
+                })
+            } else {
+                this.setState({
+                    loggedIn: false
+                })
+            }
+        });
     }
+    renderContent() {
+        switch (this.state.loggedIn){
+            case true:
+                return (<Button style={{marginBottom: 50, marginTop: 50}} onPress={() => firebase.auth().signOut()}>Log Out</Button>)
+            case false:
+                return <LoginForm />
+            default:
+                return <Spinner size='large' />
+        }
+    }
+
+
 
 
     render() {
         return (
         <View>
             <Header text={'Wild5'}/>
-            <LoginForm />
+            <CardSection>{this.renderContent()}</CardSection>
         </View>
         )
   }
