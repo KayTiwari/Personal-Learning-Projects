1dfbb47f08bfe78ef113f49bc20cf40c7ce11d77
Abhinav Tiwari <abhi.kay.tiwari@gmail.com>
2019-04-20 15:44:55 -0600

    techstack: added reducers and common directory

---
+++ reactnativetut/techstack/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { View } from 'react-native';
+import { Provider } from 'react-redux';
+import reducers from './reducers'
+import { CreateStore } from 'redux'
+
+const App = () => {
+    return (
+        <Provider store={createStore(reducers)}>
+        <View></View>
+        </Provider>
+    );
+};
+
+export default App;
