653d5aefb1ab888d22872af477310a4714cf6b15
Abhinav Tiwari <abhi.kay.tiwari@gmail.com>
2019-04-20 11:57:08 -0600

    changes towards user auth index

---
+++ reactnativetut/auth/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+import { Header } from './components/common';
+
+export default class App extends Component {
+  render() {
+    return (
+      <View>
+          <Header text={'Wild5'}/>
+          <Text>Hi</Text>
+      </View>
+    )
+  }
+}
