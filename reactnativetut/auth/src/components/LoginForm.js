78a54f8bc0493d852709beda01d7a31297ba4d6c
Abhinav Tiwari <abhi.kay.tiwari@gmail.com>
2019-04-20 13:26:40 -0600

    added firebase user auth success and failure procedures

--- reactnativetut/auth/src/components/LoginForm.js
+++ reactnativetut/auth/src/components/LoginForm.js
@@ -1,13 +1,30 @@
 import React, {Component} from 'react';
-import { View, TextInput } from 'react-native';
+import { Text } from 'react-native';
 import { Button, Card, CardSection, Input } from './common';
+import firebase from 'firebase'
 
 class LoginForm extends Component {
 
     state = {
         email: '',
-        pass: ''
+        password: '',
+        error: ''
     } 
+    onButtonPress() {
+        const { email, password } = this.state
+        this.setState({
+            error: ''
+        })
+        firebase.auth().signInWithEmailAndPassword(email, password)
+            .catch(() => {
+                firebase.auth().createUserWithEmailAndPassword(email, password)
+                .catch(() => {
+                    this.setState({
+                        error: 'Authentication failed'
+                    })
+                });
+            });
+    }
 
     render(){
         return (
@@ -18,6 +35,7 @@ class LoginForm extends Component {
                 label='Email'
                 value={this.state.email}
                 onChangeText={email => this.setState({ email })}
+                
                 />
             </CardSection>
 
@@ -25,18 +43,30 @@ class LoginForm extends Component {
             <Input
                 placeholder='Enter your password'
                 label='Password'
-                value={this.state.pass}
-                onChangeText={pass => this.setState({ pass })}
+                value={this.state.password}
+                onChangeText={password => this.setState({ password })}
+                secureTextEntry
                 />
             </CardSection>
 
+            <Text style={styles.errorTextStyle}>
+                {this.state.error}
+            </Text>
+
             <CardSection>
-                <Button>
+                <Button onPress={this.onButtonPress.bind(this)}>
                     Login
                 </Button>
             </CardSection>
         </Card>
         )
+    }
+}
+const styles = {
+    errorTextStyle: {
+        fontSize: 20,
+        alignSelf: 'center',
+        color: '#f00'
     }
 }
 
