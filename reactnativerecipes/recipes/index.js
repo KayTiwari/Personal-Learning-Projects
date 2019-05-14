f0d9a42fc75c449fb7abbb82be4379992da3d3b2
Abhinav Tiwari <abhi.kay.tiwari@gmail.com>
2019-05-14 15:37:44 -0600

    app is registered and displaying on sim

--- reactnativerecipes/recipes/index.js
+++ reactnativerecipes/recipes/index.js
@@ -1,9 +1,8 @@
 /**
  * @format
  */
-
+import React, {Component} from 'react';
 import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
+import MainApp from './src/MainApp';
 
-AppRegistry.registerComponent('Recipes', () => App);
+AppRegistry.registerComponent('recipes', () => MainApp);
--- reactnativerecipes/recipes/src/MainApp.js
+++ reactnativerecipes/recipes/src/MainApp.js
@@ -3,17 +3,45 @@ import { StyleSheet, Text, View } from 'react-native';
 
 
 const MainApp = () => {
-    const name = '01 - Hey, this is my life';
+    const name = '01 - Dark Side Of The Moon';
 
     return (
         <View style={styles.container}>
-        <View style={styles.innerContainer}>
+        <View style={styles.innerContainer} />
         <Text style={styles.title}>
         <Text style={styles.subtitle}>Playing:</Text> {name}
         </Text>
         </View>
-        </View>
     );
 };
 
+
+
+const styles = StyleSheet.create({
+    container: {
+        margin: 10,
+        marginTop: 100,
+        backgroundColor: '#e67e22',
+        borderRadius: 5,
+    },
+    innerContainer: {
+        backgroundColor: '#d35400',
+        height: 50,
+        width: 150,
+        borderTopLeftRadius: 5,
+        borderBottomLeftRadius: 5
+    },
+    title: {
+        fontSize: 18,
+        fontWeight: '200',
+        color: '#fff',
+        position: 'absolute',
+        backgroundColor: 'transparent',
+        top: 12,
+        left: 10,
+    },
+    subtitle: {
+        fontWeight: 'bold',
+    }
+});
 export default MainApp;
