4c3d73e03cca1ab3892a0c496b004d49b0eb0a26
Abhinav Tiwari <abhi.kay.tiwari@gmail.com>
2019-05-14 16:13:02 -0600

    removed old projects for space

--- reactnativerecipes/recipes/index.js
+++
@@ -1,8 +0,0 @@
-/**
- * @format
- */
-import React, {Component} from 'react';
-import {AppRegistry} from 'react-native';
-import MainApp from './src/MainApp';
-
-AppRegistry.registerComponent('recipes', () => MainApp);
