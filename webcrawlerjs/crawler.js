7081af58c5a72be7b6ac9340774f1d228c35ca62
Abhinav Tiwari <abhi.kay.tiwari@gmail.com>
2019-05-24 15:14:49 -0600

    web crawler visits ars technica and returns pageand returns page title

--- webcrawlerjs/crawler.js
+++ webcrawlerjs/crawler.js
@@ -6,3 +6,21 @@ var cheerio = require('cheerio');
 
 var URL = require('url-parse')
 //Used to parse urls
+
+
+var pageToVisit = "http://www.arstechnica.com";
+console.log('Visiting page: ' + pageToVisit);
+
+request(pageToVisit, function(error, response, body){
+    if(error){
+        console.log('Error: ' + error)
+    }
+    console.log('Status code: ' + response.statusCode);
+    if (response.statusCode === 200){
+        var $ = cheerio.load(body);
+        //parses doc body
+
+        console.log("Page title:  " + $('title').text());
+        //Selects html element of <title> and displays text within it
+    }
+})
