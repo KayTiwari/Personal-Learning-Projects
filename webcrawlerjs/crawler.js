c04d1ab263efc6c8f802039ff514c1072dcc76b4
Abhinav Tiwari <abhi.kay.tiwari@gmail.com>
2019-05-24 15:07:16 -0600

    added package json with dependencies

--- webcrawlerjs/crawler.js
+++ webcrawlerjs/crawler.js
@@ -1,3 +1,8 @@
 var request = require('request');
+//Used to make http requests
+
 var cheerio = require('cheerio');
+//Used to parse and select HTML elements on the page
+
 var URL = require('url-parse')
+//Used to parse urls
