6099f24e5a09c0d451146fbf7608bf52ec3e8f72
Abhinav Tiwari <abhi.kay.tiwari@gmail.com>
2019-05-25 17:58:59 -0600

    added a bitscraper

---
+++ webcrawlerjs/dynamicwebscraper.js
@@ -0,0 +1,4 @@
+const nightmare = require('nightmare')
+//browser automation library
+const request = require('request');
+const cheerio = require('cheerio');
--- webcrawlerjs/wikiscraper.js
+++ webcrawlerjs/wikiscraper.js
@@ -1,15 +1,17 @@
 const rp = require('request-promise');
-const url = 'https://en.wikipedia.org/wiki/List_of_Presidents_of_the_United_States';
+const url = 'https://www.ksl.com/auto/search/index?priceFrom=1&priceTo=4000&zip=84009&miles=25&newUsed%5B%5D=New&newUsed%5B%5D=Used&newUsed%5B%5D=Certified';
 const $ = require('cheerio');
 
 rp(url)
 .then(function(html){
-    let wikiUrls = [];
-    // console.log($('big > a', html).length);
-    for (let i = 0; i < ($('big > a', html).length); i++){
-        wikiUrls.push($('big > a', html)[i].attribs.href)
-    }
-    console.log(wikiUrls);
+    console.log(html);
+    // console.log($);
+    // let wikiUrls = [];
+    // // console.log($('big > a', html).length);
+    // for (let i = 0; i < ($('big > a', html).length); i++){
+    //     wikiUrls.push($('big > a', html)[i].attribs.href)
+    // }
+    // console.log(wikiUrls);
 })
 .catch(function(error){
     console.log(error);
