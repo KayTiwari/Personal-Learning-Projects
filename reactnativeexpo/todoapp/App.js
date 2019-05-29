e986d0dcbafde7b258457fd35512134a67d62a04
Abhinav Tiwari <abhi.kay.tiwari@gmail.com>
2019-05-28 21:21:14 -0600

    course is too basic

--- reactnativeexpo/todoapp/App.js
+++ reactnativeexpo/todoapp/App.js
@@ -1,15 +1,23 @@
 import React, {Component} from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
 class App extends Component {
   state = {
-
+    todo: 'message'
   }
 
   render(){
     return(
       <View style={styles.viewStyles}>
         <Text>fuck you</Text>
+        <TextInput 
+          style={styles.textInput}
+          onChangeText={(text) => this.setState({todo: text})}
+        />
+        <Button 
+        title='Add Todo'
+        />
+        <Text>{this.state.todo}</Text>
       </View>
     )
   }
@@ -23,5 +31,11 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center'
+  },
+  textInput: {
+    height: 40,
+    borderColor: 'black',
+    borderWidth: 1,
+    width: '50%'
   }
 })
