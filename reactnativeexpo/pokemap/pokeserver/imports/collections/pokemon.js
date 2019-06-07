78038feec0b638e36e0576ba7b3b31f71875765b
Abhinav Tiwari <abhi.kay.tiwari@gmail.com>
2019-06-06 21:58:44 -0600

    removed react native stuff to its own dir and added marketcheck api price checker

--- reactnativeexpo/pokemap/pokeserver/imports/collections/pokemon.js
+++
@@ -1,9 +0,0 @@
-import {Mongo} from 'meteor/mongo';
-import {Meteor} from 'meteor/meteor';
-
-export const Pokemon = new Mongo.Collection('pokemon');
-
-Meteor.publish('pokemon', () => {
-    return Pokemon.find({});
-});
-
--- reactnativetut/techstack/src/components/common/index.js
+++
@@ -1,6 +0,0 @@
-export * from './Button';
-export * from './CardSection';
-export * from './Card';
-export * from './Header';
-export * from './Input';
-export * from './Spinner';
